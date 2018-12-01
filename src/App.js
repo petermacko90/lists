ba5abc55e1cbd29bc06cf1583692fd91dd9d6962
Peter Macko <petermacko90@gmail.com>
2018-12-01 19:51:51 +0100

    moved redux from App to Lists

--- src/App.js
+++ src/App.js
@@ -1,71 +1,18 @@
 import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { requestLists, setCurrentList } from './actions/lists';
-import { requestItems, setCurrentItems } from './actions/items';
 import Navigation from './components/Navigation/Navigation';
 import Lists from './components/Lists/Lists';
 import CurrentList from './components/CurrentList/CurrentList';
-import './App.css';
-
-const mapStateToProps = (state) => {
-  return {
-    lists: state.listsReducer.lists,
-    items: state.itemsReducer.items
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onRequestLists: () => dispatch(requestLists()),
-    onRequestItems: () => dispatch(requestItems()),
-    onSetCurrentList: (list) => dispatch(setCurrentList(list)),
-    onSetCurrentItems: (items) => dispatch(setCurrentItems(items))
-  }
-}
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestLists();
-    this.props.onRequestItems();
-  }
-
-  /* handle selecting a list */
-  onClickList = (list, listId) => (e) => {
-    this.handleSelectList(list, listId);
-  }
-
-  onEnterList = (list, listId) => (e) => {
-    if (e.key === 'Enter') {
-      this.handleSelectList(list, listId);
-    }
-  }
-
-  handleSelectList = (list, listId) => {
-    this.props.onSetCurrentList(list);
-    this.props.onSetCurrentItems(listId);
-  }
-
   render() {
-    const { lists, items } = this.props;
-
     return (
       <Fragment>
         <Navigation />
-        {
-          lists.length > 0 ?
-            <Lists
-              lists={lists}
-              items={items}
-              onClickList={this.onClickList}
-              onEnterList={this.onEnterList}
-            />
-          :
-            <p className="message">No lists found</p>
-        }
+        <Lists />
         <CurrentList />
       </Fragment>
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -1,34 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import List from '../List/List';
 import './Lists.css';
+import { requestLists, setCurrentList } from '../../actions/lists';
+import { requestItems, setCurrentItems } from '../../actions/items';
 
-const Lists = ({ lists, items, onClickList, onEnterList }) => {
-	let listComponenets = [];
-	let itemsProp = [];
-	
-	lists.forEach((list) => {
-		items.forEach((item) => {
-			if (list.id === item.list_id) {
-				itemsProp = itemsProp.concat(item);
-			}
-		});
-		listComponenets = listComponenets.concat(
-			<List
-				key={list.id}
-				list={list}
-				items={itemsProp}
-				onClickList={onClickList}
-				onEnterList={onEnterList}
-			/>
-		);
-		itemsProp = [];
-	});
-
-	return (
-		<div className="fl w-25-l w-third-m w-100 lists">
-			{listComponenets}
-		</div>
-	);
+const mapStateToProps = (state) => {
+  return {
+    lists: state.listsReducer.lists,
+    items: state.itemsReducer.items
+  }
 }
 
-export default Lists;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRequestLists: () => dispatch(requestLists()),
+    onRequestItems: () => dispatch(requestItems()),
+    onSetCurrentList: (list) => dispatch(setCurrentList(list)),
+    onSetCurrentItems: (items) => dispatch(setCurrentItems(items))
+  }
+}
+
+class Lists extends Component {
+  componentDidMount() {
+    this.props.onRequestLists();
+    this.props.onRequestItems();
+  }
+
+  /* handle selecting a list */
+  onClickList = (list, listId) => (e) => {
+    this.handleSelectList(list, listId);
+  }
+
+  onEnterList = (list, listId) => (e) => {
+    if (e.key === 'Enter') {
+      this.handleSelectList(list, listId);
+    }
+  }
+
+  handleSelectList = (list, listId) => {
+    this.props.onSetCurrentList(list);
+    this.props.onSetCurrentItems(listId);
+  }
+
+  render() {
+    const { lists, items } = this.props;
+    let listComponents = [];
+    let itemsProp = [];
+
+    lists.forEach((list) => {
+      items.forEach((item) => {
+        if (list.id === item.list_id) {
+          itemsProp = itemsProp.concat(item);
+        }
+      });
+      listComponents = listComponents.concat(
+        <List
+          key={list.id}
+          list={list}
+          items={itemsProp}
+          onClickList={this.onClickList}
+          onEnterList={this.onEnterList}
+        />
+      );
+      itemsProp = [];
+    });
+
+    return (
+      <div className="fl w-25-l w-third-m w-100 lists">
+        {
+          listComponents.length > 0
+            ? listComponents
+            : <p className="message">No lists found</p>
+        }
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Lists);
