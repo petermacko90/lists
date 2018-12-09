fd34b34a389bec40877d57adabdc7d84ba298ea5
Peter Macko <petermacko90@gmail.com>
2018-12-09 11:26:20 +0100

    Updates

---
+++ precache-manifest.cb8f1c8761f24c791822629e9fc8a545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93bb08ede66c31d15183",
+    "url": "/lists/static/js/runtime~main.93bb08ed.js"
+  },
+  {
+    "revision": "db9a15b52e8bdaeafce5",
+    "url": "/lists/static/js/main.db9a15b5.chunk.js"
+  },
+  {
+    "revision": "3fd9fc4eea69338afe77",
+    "url": "/lists/static/js/1.3fd9fc4e.chunk.js"
+  },
+  {
+    "revision": "db9a15b52e8bdaeafce5",
+    "url": "/lists/static/css/main.63c4eb9b.chunk.css"
+  },
+  {
+    "revision": "3fd9fc4eea69338afe77",
+    "url": "/lists/static/css/1.cc5ff46a.chunk.css"
+  },
+  {
+    "revision": "0675981be072e5f38d8273b8e32938c0",
+    "url": "/lists/index.html"
+  }
+];
