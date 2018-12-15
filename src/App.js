55ea6773875cdedbde51bd600ffc795fef2aa4b1
Peter Macko <petermacko90@gmail.com>
2018-12-15 09:59:08 +0100

    register service worker, added icons

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App extends Component {
           <div ref={(ref) => this.scroll = ref} />
           <CurrentList />
         </div>
+        <div className="ma3 tc">
+          Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">
+          Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">
+          CC 3.0 BY</a>
+        </div>
       </Fragment>
     );
   }
