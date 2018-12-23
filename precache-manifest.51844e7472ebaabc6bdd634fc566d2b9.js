2074b2ea45cc573219914c9581901de3281c803e
Peter Macko <petermacko90@gmail.com>
2018-12-23 20:55:27 +0100

    Updates

--- precache-manifest.51844e7472ebaabc6bdd634fc566d2b9.js
+++ precache-manifest.51844e7472ebaabc6bdd634fc566d2b9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/lists/static/js/runtime~main.93bb08ed.js"
   },
   {
-    "revision": "141f604fc62ef77e628f",
-    "url": "/lists/static/js/main.141f604f.chunk.js"
+    "revision": "c05d6e422c76581cae86",
+    "url": "/lists/static/js/main.c05d6e42.chunk.js"
   },
   {
     "revision": "3fd9fc4eea69338afe77",
     "url": "/lists/static/js/1.3fd9fc4e.chunk.js"
   },
   {
-    "revision": "141f604fc62ef77e628f",
+    "revision": "c05d6e422c76581cae86",
     "url": "/lists/static/css/main.87234a7d.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/lists/static/css/1.cc5ff46a.chunk.css"
   },
   {
-    "revision": "accda6a64ac7fa5c807404382c787d7c",
+    "revision": "fa8db561af415b90c0dd0f1c26dfb6b0",
     "url": "/lists/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lists/precache-manifest.ea33e587aace8675d36405b2261c31ca.js"
+  "/lists/precache-manifest.51844e7472ebaabc6bdd634fc566d2b9.js"
 );
 
 workbox.clientsClaim();
