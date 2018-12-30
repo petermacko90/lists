ea606c941cbae56733374b14f01b37919a9f052f
Peter Macko <petermacko90@gmail.com>
2018-12-30 11:19:31 +0100

    small changes to List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,12 +3,12 @@ import './List.css';
 
 const List = ({ list, items, onClickList, onEnterList }) => {
 	return (
-		<div className="pa2 ma2 mt1 bg-yellow pointer shadow-3 noselect list"
+		<div className="pa2 ma3 bg-yellow pointer shadow-3 noselect list"
 		tabIndex="0" onClick={onClickList(list, list.id)}
 		onKeyPress={onEnterList(list, list.id)}>
 			<h3 className="f3-l f4-m f5 truncate">{list.title}</h3>
 			<p>{list.modified.toLocaleDateString()}</p>
-			<p className="list-items truncate">
+			<p className="truncate">
 				{
 					items.slice(0, 5).map((item) => {
 						return <span key={item.id}>- {item.name} </span>;
