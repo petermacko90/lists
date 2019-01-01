54359cab544960a1bd6dc5a8e99aa479f5769835
Peter Macko <petermacko90@gmail.com>
2019-01-01 11:25:03 +0100

    replace title with logo in Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,9 +4,9 @@ import './Navigation.css';
 const Navigation = ({ showAddList }) => {
   return (
     <nav className="bg-yellow shadow-2 mb2">
-      <h1 className="dib f2 pa3 mv0">Lists</h1>
+      <img src="list32.png" alt="logo" className="ma3 v-mid" />
       <button type="button" onClick={showAddList}
-      className="b--none ml2 bg-transparent b pointer add-list">
+      className="b--none ml2 bg-transparent b pointer v-mid add-list">
         <span className="f2">+</span>
         <span className="f3 dn di-ns"> Add list</span>
       </button>
