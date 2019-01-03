2a6712f22c23d6b0e605f9d5867bcad5569d2765
Peter Macko <petermacko90@gmail.com>
2019-01-03 22:25:18 +0100

    add check/uncheck to item actions

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -23,11 +23,11 @@ class Item extends Component {
   render() {
     const { id, listId, checked, name, onClick, onClickDelete } = this.props;
     const { isActionsOpen } = this.state;
+    const checkAction = checked ? 'Uncheck' : 'Check';
 
     return (
       <li className={"noselect" + (checked ? ' checked' : '')} tabIndex="0"
-      title={checked ? 'Uncheck' : 'Check'}
-      onClick={onClick(listId, id, checked)}>
+      title={checkAction} onClick={onClick(listId, id, checked)}>
         {name}
         <div className="absolute right-0 top-0" onBlur={this.onBlur}
         onFocus={this.onFocus}>
@@ -38,6 +38,10 @@ class Item extends Component {
           {
             isActionsOpen &&
               <div className="actions-content w4 shadow-3">
+                <button type="button" onClick={onClick(listId, id, checked)}
+                className="w-100 pointer" title={checkAction}>
+                  {checkAction}
+                </button>
                 <button type="button" onClick={onClickDelete(listId, id)}
                 className="w-100 pointer" title="Delete item">
                   Delete item
