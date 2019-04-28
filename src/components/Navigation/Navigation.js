8c132a48e7b264f57c1f8c2b00583fbb28743a3d
Peter Macko <petermacko90@gmail.com>
2019-04-28 12:55:26 +0200

    changes in Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,10 +7,16 @@ import { STR_ADD_LIST } from '../../constants/strings';
 
 const Navigation = ({ showAddList }) => {
   return (
-    <nav className="bg-yellow shadow-2 mb2">
-      <img src={logo} alt="Lists" className="ma3 v-mid" />
-      <button type="button" onClick={showAddList}
-      className="b--none ml2 bg-transparent f3 b pointer v-mid add-list">
+    <nav className="bg-yellow shadow-2 mb2 flex justify-between">
+      <div>
+        <img src={logo} alt="logo" className="ma3 v-mid" />
+        <h1 className="dib f2 mv2 v-mid">Lists</h1>
+      </div>
+      <button
+        type="button"
+        onClick={showAddList}
+        className="b--none ml4 mr3 bg-transparent f3 b pointer v-mid add-list"
+      >
         <span><FontAwesomeIcon icon={faPlus} /></span>
         <span className="dn di-ns"> {STR_ADD_LIST}</span>
       </button>
