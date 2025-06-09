5207c7e03ed0f2c057bb0fe638b8dc46151c399b
Peter Macko <peter.macko@accenture.com>
2025-06-09 15:02:13 +0200

    add file types to VitePWA config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,json}'],
+      },
+    }),
+  ],
   build: {
     outDir: 'build',
   },
