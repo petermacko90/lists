bf976a9e1c0452a589ffb77a630c0e0214c10421
Peter Macko <peter.macko@accenture.com>
2025-06-09 11:29:23 +0200

    move sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,7 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read http://bit.ly/CRA-PWA
 
-import { strings } from './constants/strings';
+import { strings } from './src/constants/strings';
 
 let str = null;
 switch (window.navigator.language) {
@@ -39,7 +39,7 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL('/lists/', window.location.href);
+    const publicUrl = new URL(import.meta.env.BASE_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -48,7 +48,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `/lists/service-worker.js`;
+      const swUrl = `${import.meta.env.BASE_URL}service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../serviceWorker';
 import 'tachyons';
 
 const root = createRoot(document.getElementById('root')!);
