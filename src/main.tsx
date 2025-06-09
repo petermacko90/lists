4e3a6988a842635b60f36fada595ae8d8fadce2c
Peter Macko <peter.macko@accenture.com>
2025-06-09 13:51:57 +0200

    add vite-plugin-pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from '../serviceWorker';
 import 'tachyons';
 
 const root = createRoot(document.getElementById('root')!);
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorker.register();
