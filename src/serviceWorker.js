556bf0fbd722cc6a6a2cc3a95fafaa47211313fc
Peter Macko <peter.macko@accenture.com>
2025-06-09 10:55:42 +0200

    fix sw url

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -48,7 +48,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `/lists/src/service-worker.js`;
+      const swUrl = `/lists/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
