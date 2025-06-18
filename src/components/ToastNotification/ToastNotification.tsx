ac80820239091a843911bde210d242ac025fb539
Peter Macko <peter.macko@accenture.com>
2025-06-18 10:51:23 +0200

    use portal for toast notification

--- src/components/ToastNotification/ToastNotification.tsx
+++ src/components/ToastNotification/ToastNotification.tsx
@@ -1,7 +1,10 @@
+import { createPortal } from 'react-dom';
+
 export default function ToastNotification({ show, text }: { show: boolean; text: string }) {
-  return (
+  return createPortal(
     <div className={'truncate toast' + (show ? ' show' : '')} role="status" aria-hidden={!show}>
       {text}
-    </div>
+    </div>,
+    document.body
   );
 }
